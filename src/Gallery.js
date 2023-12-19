36ecec0ae5630f9f675ce96f2be51c2ea2483291
afrooz Shaddel <afrooz880@gmail.com>
2023-12-19 16:55:13 +0330

    add gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,8 +2,7 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FaArrowLeftLong } from "react-icons/fa6";
-import { FaArrowRight } from "react-icons/fa6";
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -16,31 +15,31 @@ import Header from './Header/Header';
 // import required modules
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 import { CiStar } from "react-icons/ci";
+
 // import { useState } from 'react';
 export default  function Gallery(){
 
   
-  let img1=[{id:1,img: "/gallery/1 (5).jpg " , name :"m"  ,rating:2.5},{id:2,img: "/gallery/1 (2).jpg"  , name :"m" ,rating:2.5}  , {id:3,img: "/gallery/Fright+Night+Scoop_3c97255e-4b86-4123-97d7-48916c48ff65-prv.jpg"  , name :"m" ,rating:2.5}   ,{id:4,img: "public\gallery\Monster-Burgers-Recipe-Halloween-Food-Ideas-Easy-Halloween-Burgers-12-scaled.jpg"  , name :"m" ,rating:2.5} ,
-          {id:5,img: "/gallery/download (3).jpg"  , name :"m" ,rating:2.5} ,
-    {id:6,img: "/gallery/exps201865_UH153291A04_29_2b.jpg"   , name :"m" ,rating:2.5}  ,
-    {id:7,img: "/gallery/Halloween-pudding-cups_3.jpg"   , name :"m" ,rating:2.5} ,
-    {id:8,img:"/gallery/images (3).jpg"  , name :"m" ,rating:2.5} , 
-    {id:9,img: "/gallery/images (4).jpg"   , name :"m" ,rating:2.5} ]
+  let img1=[{id:1,img: "/gallery/5.jpg " , name :"spooky"  ,rating:2.5},
+  {id:2,img: "/gallery/IMG_5089.jpg"  , name :"spine" ,rating:2.5}  , 
+  {id:3,img: "/gallery/Fright+Night+Scoop_3c97255e-4b86-4123-97d7-48916c48ff65-prv.jpg"  , name :"Vegan" ,rating:2.5}  
+  ,{id:4,img: "/gallery/images (4).jpg"  , name :"Secretly" ,rating:2.5} ,
+  {id:5,img: "/gallery/download (3).jpg"  , name :"Shortbread" ,rating:2.5} ,
+    {id:6,img: "/gallery/images (2).jpg"   , name :"Lunch Box" ,rating:2.5}  ,
+    {id:7,img: "/gallery/Halloween-pudding-cups_3.jpg"   , name :" Scary halloween" ,rating:2.5} ,
+    {id:8,img:"/gallery/images (3).jpg"  , name :"finger food" ,rating:2.5} , 
+    {id:9,img: "/gallery/1 (2).jpg"   , name :"floady" ,rating:2.5} ]
 
  
     return(
         <>
         <div id='tranding'>
 <div className='container1'>
-  {/* <h2 className='text-center section-subheading'>Gallery</h2> */}
   <Header  item="G A L l E R Y"/>
-  
   </div>
-          
                  
-                 
-                 <div className='container1'>
-                 <Swiper
+     <div className='container1'>
+        <Swiper
         cssMode={true}
         navigation={true}
         pagination={true}
@@ -54,317 +53,50 @@ export default  function Gallery(){
           stretch: 0,
           depth: 100,
           modifier: 2.5,
-          slideShadows: true,
+          
         }}
         grabCursor={true}
-        slidesPerView={'auto'}
-       
-        
-       
-        
-       
-        
+        slidesPerView='auto'
+      
         modules={[Navigation,EffectCoverflow, Mousewheel, Keyboard]}
         className="mySwiper  tranding-slider"
       >
+        {img1.map(item=>(
+           <SwiperSlide className='tranding-slide'>
+           <div className='tranding-slide-img'>
+               <img src={item.img} alt="tranding" />
+ 
+           </div>
+           <div className='tranding-slide-content'>
+            
+             <div className='tranding-slide-content-bottom'>
+               <h2 className='food-name'>
+               {item.name}
+ 
+               </h2>
+               <h3 className='food-rating'>
+                 <span>{item.rating}</span>
+                 <div className='rating'>
+                 <CiStar className='star'/>
+                 <CiStar className='star'/>
+                 <CiStar className='star'/>
+                 <CiStar className='star'/>
+                 <CiStar className='star'/>
+ 
+ 
+                 </div>
+ 
+               </h3>
+ 
+             </div>
+ 
+           </div>
+ 
+           </SwiperSlide>
+
+        ))}
       
-         <SwiperSlide className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\5.jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-              spooky
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\IMG_5089.jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-             spine
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\images (4).jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-              Vegan
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\images (3).jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-              Secretly
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\Halloween-pudding-cups_3.jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-             Shortbread
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\Peanut-Butter-Rice-Krispie-Treat-Pumpkins-Recipe-4.jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-             Lunch Box
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\download (3).jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-              Spooky
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\images (2).jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-             Finger food
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-
-          <SwiperSlide  className='tranding-slide'>
-          <div className='tranding-slide-img'>
-              <img src="\gallery\Fright+Night+Scoop_3c97255e-4b86-4123-97d7-48916c48ff65-prv.jpg" alt="tranding" />
-
-          </div>
-          <div className='tranding-slide-content'>
-           
-            <div className='tranding-slide-content-bottom'>
-              <h2 className='food-name'>
-              Scary halloween
-
-              </h2>
-              <h3 className='food-rating'>
-                <span>2.5</span>
-                <div className='rating'>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-                <CiStar className='star'/>
-
-
-                </div>
-
-              </h3>
-
-            </div>
-
-          </div>
-
-          </SwiperSlide>
-      
-                  {/* <div className='tranding-slider-control'>
-                    <div className="swiper-button-prev slider-arrow">
-                    <FaArrowLeftLong/> 
-                    </div>
-
-                    <div className="swiper-button-next slider-arrow">
-                    <FaArrowRight/> 
-                    </div>
-                  </div> */}
+                
           </Swiper>
 
   </div>
@@ -373,13 +105,7 @@ export default  function Gallery(){
                  
    </div>              
                  
-                 
-                 
-                 
-                 
-                 
-                 
-                 
+              
        
     </>
 
--- src/holloween/AutoPlay/AutoPlay.js
+++ src/holloween/AutoPlay/AutoPlay.js
@@ -17,6 +17,16 @@ import './AutoPlay.css';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 export default function AutoPlay() {
+  let img=["/remove/New folder (2)/pizza-food-monster-graphic-illustration-260nw-1543141172.png","/remove/360_F_635646268_68UfNglAzkljsJk5si30KF8geXVPzKEx.png" ,
+  "/remove/images (4).png",
+  "/remove/images (8).png",
+  "/remove/images (9).png",
+  "/remove/New folder (2)/images (7).png",
+"/remove/New folder (2)/there-is-glass-orange-juice-with-pumpkins-it-generative-ai_900396-38403.png" ,
+"/remove/there-is-cupcake-with-halloween-decoration-top-generative-ai_1034638-402.png",
+,"/remove/New folder (2)/pngtree-pizza-with-olives-and-sausage-spider-for-halloween-party-png-image_10301020.png"
+,"/remove/New folder (2)/106223985.png"
+]
   return (
     <>
       <Swiper
@@ -30,22 +40,14 @@ export default function AutoPlay() {
       
        
        
+       {img.map((item , index)=>(
+        <SwiperSlide key={index}><img className='autoplaypicture'  src={item} alt="" /></SwiperSlide> 
+       ))}
        
        
-       
-      <SwiperSlide><img className='autoplaypicture'  src="\remove\New folder (2)\there-is-glass-orange-juice-with-pumpkins-it-generative-ai_900396-38403.png" alt="" /></SwiperSlide>
+   
     
-      <SwiperSlide><img className='autoplaypicture'  src="\remove\New folder (2)\pizza-food-monster-graphic-illustration-260nw-1543141172.png" alt="" /></SwiperSlide>
-      <SwiperSlide><img className='autoplaypicture'  src="\remove\New folder (2)\pngtree-pizza-with-olives-and-sausage-spider-for-halloween-party-png-image_10301020.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture'  src="\remove\New folder (2)\106223985.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\360_F_635646268_68UfNglAzkljsJk5si30KF8geXVPzKEx.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\images (3).png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\images (4).png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\images (8).png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\images (9).png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\images (10).png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture' src="\remove\there-is-cupcake-with-halloween-decoration-top-generative-ai_1034638-402.png" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='autoplaypicture'  src="\remove\New folder (2)\images (7).png" alt="" /></SwiperSlide>
+    
       </Swiper>
     </>
   );
